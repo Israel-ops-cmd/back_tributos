ad037c0040c470d0b012e580d2a69450d0a7ed02
Israel-ops-cmd <israellipe2020@gmail.com>
2025-09-18 11:52:31 -0300

    feat: add API error handling middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes/index.js'
+import 'express-async-errors'
+import ErrorHandleMiddleware from '@shared/middlewares/ErrorHandleMiddleware.js'
 
 const app = express()
 
@@ -8,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+app.use(ErrorHandleMiddleware.handleError)
 
 app.listen(3334, () => {
   console.log('Server started on port 3334!')
