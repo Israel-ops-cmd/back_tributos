004438ae886ad4431a8e5645b8186f73b5cdf1c4
Israel-ops-cmd <israellipe2020@gmail.com>
2025-09-19 10:27:47 -0300

    feat: create leads entity

---
+++ src/modules/leads/database/entities/Leads.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity('leads')
+export class Leads {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar'})
+  name!: string;
+
+  @Column({ type: 'varchar'})
+  email!: string;
+
+  @Column({ type: 'text', nullable: true})
+  message?: string;
+
+  @Column({ type: 'timestamp', default: () => 'now()'})
+  created_at!: Date;
+}
