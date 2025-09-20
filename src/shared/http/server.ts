e267a750b367b5f23ccabb52d306e369c0142131
Israel-ops-cmd <israellipe2020@gmail.com>
2025-09-19 23:50:53 -0300

    feat: lead data validation

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import routes from './routes/index.js'
 import 'express-async-errors'
 import ErrorHandleMiddleware from '../middlewares/ErrorHandleMiddleware.js'
 import { AppDataSource } from '../typeorm/data-source.js'
+import { errors } from 'celebrate'
 
 AppDataSource.initialize().then(async () => {
   const app = express()
@@ -13,6 +14,7 @@ AppDataSource.initialize().then(async () => {
   app.use(express.json())
 
   app.use(routes)
+  app.use(errors())
   app.use(ErrorHandleMiddleware.handleError)
 
   console.log('Connected to the database!')
